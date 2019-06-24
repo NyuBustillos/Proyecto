2ccd64cbfb55d3b5d5e3d6eb777fbf74cc162082
Adri_bq <nyubustillos@gmail.com>
2019-06-24 01:06:41 -0400

    3er commit. Crear la página donde se agrega elementos vendidos

---
+++ Vendido/Vendido/Vendido/Views/Pagina.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vendido.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina : ContentPage
+    {
+        public Pagina()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
